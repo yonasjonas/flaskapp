0101e52b1a3cc244a3df42f6936d607c6e8d93aa
Jonas Samaitis <32172586+yonasjonas@users.noreply.github.com>
2020-03-16 23:31:03 +0000

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+
 sudo apt-get -y update
 mkdir py
 cd py
@@ -6,7 +7,7 @@ sudo apt-get -y install python3-pip
 sudo pip3 install virtualenv
 virtualenv venv
 source venv/bin/activate
-pip install flask 
+pip install flask
 sudo apt-get -y install gcc python3-dev
-pip install psutil 
+pip install psutil
 git clone https://github.com/yonasjonas/flaskapp.git
